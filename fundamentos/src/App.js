70fd52bb1376b2fbfb33847259cfd9ed90293f6a
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-05-14 11:45:48 -0300

    feat: apply coditional rederization

--- fundamentos/src/App.js
+++ fundamentos/src/App.js
@@ -5,10 +5,34 @@ import Post from "./Post";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: "Title#01", subtitle: "Sub#01", likes: 20 },
-    { id: Math.random(), title: "Title#02", subtitle: "Sub#02", likes: 10 },
-    { id: Math.random(), title: "Title#03", subtitle: "Sub#03", likes: 50 },
-    { id: Math.random(), title: "Title#04", subtitle: "Sub#04", likes: 50 },
+    {
+      id: Math.random(),
+      title: "Title#01",
+      subtitle: "Sub#01",
+      likes: 20,
+      read: false,
+    },
+    {
+      id: Math.random(),
+      title: "Title#02",
+      subtitle: "Sub#02",
+      likes: 10,
+      read: true,
+    },
+    {
+      id: Math.random(),
+      title: "Title#03",
+      subtitle: "Sub#03",
+      likes: 50,
+      read: false,
+    },
+    {
+      id: Math.random(),
+      title: "Title#04",
+      subtitle: "Sub#04",
+      likes: 50,
+      read: false,
+    },
   ]);
 
   function handleRefresh() {
@@ -38,16 +62,7 @@ function App() {
       <hr />
 
       {posts.map((post) => (
-        <Post
-          key={post.id}
-          onRemove={handleRemovePost}
-          post={{
-            id: post.id,
-            title: post.title,
-            subtitle: post.subtitle,
-          }}
-          likes={post.likes}
-        />
+        <Post key={post.id} onRemove={handleRemovePost} post={post} />
       ))}
 
       {/* <Post
