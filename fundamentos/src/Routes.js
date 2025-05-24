6d0d0c5d51e6d5b53bbd629eda83cafd3e91b52f
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-05-24 11:17:56 -0300

    feat: add animation when user change page

--- fundamentos/src/Routes.js
+++ fundamentos/src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, useLocation } from "react-router-dom";
+import { animated, useTransition } from "react-spring";
 
 import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFount";
@@ -7,14 +8,35 @@ import Post from "./Pages/Post";
 import Posts from "./Pages/Posts";
 
 export default function Routes() {
-  return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/posts" component={Posts} />
-      <Route path="/posts/:id" component={Post} />
-      <Route component={NotFound} />
-    </Switch>
-  );
+  const location = useLocation();
+  const transitions = useTransition(location, {
+    from: {
+      opacity: 0,
+      transform: "translateY(50px)",
+      position: "absolute",
+    },
+    enter: {
+      opacity: 1,
+      transform: "translateY(0)",
+      position: "absolute",
+    },
+    leave: {
+      opacity: 0,
+      transform: "translateY(50px)",
+      position: "absolute",
+    },
+  });
+
+  return transitions((props, item) => (
+    <animated.div style={props}>
+      <Switch location={item}>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/posts" component={Posts} />
+        <Route path="/posts/:id" component={Post} />
+        <Route component={NotFound} />
+      </Switch>
+    </animated.div>
+  ));
 }
 
 /*
