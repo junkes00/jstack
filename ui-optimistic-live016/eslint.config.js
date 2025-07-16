6ef60d538b9ec76ce34fe48c937218977e8e5374
Nicolas Junkes <nicolas.junkes@mouts.info>
2025-07-16 15:01:27 -0300

    feat: config shadcn

--- ui-optimistic-live016/eslint.config.js
+++ ui-optimistic-live016/eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
@@ -8,12 +8,15 @@ export default defineConfig([
   { files: ['**/*.{js,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
+
   stylistic.configs.customize({
     indent: 2,
     quotes: 'single',
     semi: true,
 
   }),
+
+  globalIgnores([".config/*"]),
 
   {
     plugins: {
