09c874b023b1aeaae02380bd172a3a05de8960d1
Nicolas Junkes <nicolas.junkes00@gmail.com>
2025-07-31 17:46:41 -0300

    feat: create error boundary component

--- next/live010/src/app/(dashboard)/makes/page.tsx
+++ next/live010/src/app/(dashboard)/makes/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 async function getMakes() {
   await new Promise((resolve) => setTimeout(resolve, 1000));
 
@@ -12,12 +14,14 @@ export default async function Makes() {
       <h1 className="mb-4 text-xl">Marcas</h1>
       <div className="grid grid-cols-5 gap-4">
         {makes.map((make) => (
-          <div
-            key={make}
-            className="bg-zinc-900 rounded-lg border border-zinc-700 grid place-items-center h-56"
-          >
-            {make}
-          </div>
+          <Link key={make} href={`/makes/${make}`}>
+            <div
+              key={make}
+              className="bg-zinc-900 rounded-lg border border-zinc-700 grid place-items-center h-56"
+            >
+              {make}
+            </div>
+          </Link>
         ))}
       </div>
     </div>
---
+++ next/live010/src/app/error.tsx
@@ -0,0 +1,5 @@
+"use client";
+
+export default function Error() {
+  return <div>Global</div>;
+}
