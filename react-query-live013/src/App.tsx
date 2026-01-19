f9b4da55057945827272c2d8891c84021a55972d
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-01-19 11:03:37 -0300

    feat: create users and posts routes

---
+++ react-query-live013/src/App.tsx
@@ -0,0 +1,39 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Posts } from './Posts';
+import { Users } from './Users';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 5000,
+      refetchOnWindowFocus: false,
+      retry: false,
+      gcTime: 10 * 60 * 1000,
+    }
+  }
+});
+
+export function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Usuários</Link>
+            <Link to="/posts">Posts</Link>
+          </li>
+        </ul>
+
+
+        <Routes>
+          <Route path="/" element={<Users />} />
+          <Route path="/posts" element={<Posts />} />
+        </Routes>
+        <ReactQueryDevtools buttonPosition="bottom-left" />
+      </BrowserRouter>
+    </QueryClientProvider>
+  )
+}
