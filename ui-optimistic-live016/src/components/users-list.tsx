c1dd0abcac4d76413d0835ec4602d4785ad54e77
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-04-11 17:30:38 -0300

    feat: add skeleton to users list

--- ui-optimistic-live016/src/components/users-list.tsx
+++ ui-optimistic-live016/src/components/users-list.tsx
@@ -1,12 +1,22 @@
 import { useUsers } from '@/app/hooks/useUsers';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 import { Switch } from './ui/switch';
+import { Skeleton } from './ui/skeleton';
 
 export function UsersList() {
-  const { users } = useUsers();
+  const { users, isLoading } = useUsers();
 
   return (
     <div className="space-y-4">
+      {isLoading && (
+        <>
+          <Skeleton className="h-20" />
+          <Skeleton className="h-20" />
+          <Skeleton className="h-20" />
+          <Skeleton className="h-20" />
+        </>
+      )}
+
       {users.map(user => (
         <div key={user.id} className="flex border p-4 rounded-md items-center justify-between">
           <div className="flex items-center gap-4">
