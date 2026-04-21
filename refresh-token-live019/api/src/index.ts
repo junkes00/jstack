a9a88c25b6c6c0a276bec0f2f101a0345142d579
Nicolas Junkes <nicolas.junkes00@gmail.com>
2026-04-21 19:11:55 -0300

    build: add fastify/cors

--- refresh-token-live019/api/src/index.ts
+++ refresh-token-live019/api/src/index.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 
+import cors from '@fastify/cors';
 import FastifyJWT from '@fastify/jwt';
 import Fastify from 'fastify';
 
@@ -8,6 +9,7 @@ import { privateRoutes, publicRoutes } from './routes';
 
 const fastify = Fastify();
 
+fastify.register(cors);
 fastify.register(FastifyJWT, {
   secret: env.JWT_SECRET,
   sign: {
